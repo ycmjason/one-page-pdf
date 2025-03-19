882da7c718f0de62c1b3787a0f23612cba8afdc3
Jason Yu <me@ycmjason.com>
2025-03-19 02:00:33 +0000

    cd

--- src/cli.ts
+++ src/cli.ts
@@ -1,15 +1,15 @@
 #!/usr/bin/env node
-import { generateSinglePagePDF } from "./generateSinglePagePDF.ts";
-import { writeFile } from "fs/promises";
+import { writeFile } from "node:fs/promises";
+import { generateSinglePagePDF } from "./generateSinglePagePDF";
 
 const url = process.argv[2];
 const outputPath = process.argv[3];
 
 if (!url || !outputPath) {
-  console.error("Usage: one-page-pdf <url> <output_file>");
-  process.exit(1);
+	console.error("Usage: one-page-pdf <url> <output_file>");
+	process.exit(1);
 }
 
-const pdfData = await generateSinglePagePDF(url)
-await writeFile(outputPath, pdfData)
-console.log(`PDF saved at: ${outputPath}`)
+const pdfData = await generateSinglePagePDF(url);
+await writeFile(outputPath, pdfData);
+console.log(`PDF saved at: ${outputPath}`);
