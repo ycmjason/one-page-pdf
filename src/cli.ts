6de6addded32d0237aa59b15b6e7ef27839dd961
Jason Yu <me@ycmjason.com>
2025-12-15 23:36:15 +0000

    bump deps

--- src/cli.ts
+++ src/cli.ts
@@ -1,18 +1,18 @@
 #!/usr/bin/env node
-import { writeFile } from "node:fs/promises";
-import { generateSinglePagePDF } from "./generateSinglePagePDF.js";
+import { writeFile } from 'node:fs/promises';
+import { generateSinglePagePDF } from './generateSinglePagePDF.js';
 
 const [_node, _script, url, outputPath] = process.argv;
 
 if (!url || !outputPath) {
-	console.error("Usage: one-page-pdf <url> <output_file>");
-	process.exit(1);
+  console.error('Usage: one-page-pdf <url> <output_file>');
+  process.exit(1);
 }
 
 if (!/http/.test(url)) {
-	console.warn(
-		`provided url \`${url}\` does not start with http:// or https://, this might cause problem...`,
-	);
+  console.warn(
+    `provided url \`${url}\` does not start with http:// or https://, this might cause problem...`,
+  );
 }
 
 const pdfData = await generateSinglePagePDF(url);
--- src/generateSinglePagePDF.ts
+++ src/generateSinglePagePDF.ts
@@ -1,32 +1,26 @@
-import puppeteer from "puppeteer";
+import puppeteer from 'puppeteer';
 
-export const generateSinglePagePDF = async (
-	url: string,
-): Promise<Uint8Array> => {
-	const browser = await puppeteer.launch({ defaultViewport: null });
-	const page = await browser.newPage();
+export const generateSinglePagePDF = async (url: string): Promise<Uint8Array> => {
+  const browser = await puppeteer.launch({
+    defaultViewport: null,
+    args: ['--no-sandbox'],
+    headless: true,
+  });
+  const page = await browser.newPage();
 
-	await page.goto(url, { waitUntil: "networkidle2" });
+  await page.goto(url, { waitUntil: 'networkidle2' });
 
-	const [pageHeight, pageWidth] = await Promise.all([
-		page.evaluate(
-			() =>
-				// @ts-ignore puppeteer uses document
-				document.documentElement.scrollHeight,
-		),
-		page.evaluate(
-			() =>
-				// @ts-ignore puppeteer uses document
-				document.documentElement.clientWidth,
-		),
-	]);
+  const [pageHeight, pageWidth] = await Promise.all([
+    page.evaluate(() => document.documentElement.scrollHeight),
+    page.evaluate(() => document.documentElement.clientWidth),
+  ]);
 
-	const pdfBuffer = await page.pdf({
-		printBackground: true,
-		width: `${pageWidth}px`,
-		height: `${pageHeight}px`,
-	});
+  const pdfBuffer = await page.pdf({
+    printBackground: true,
+    width: `${pageWidth}px`,
+    height: `${pageHeight}px`,
+  });
 
-	await browser.close();
-	return pdfBuffer;
+  await browser.close();
+  return pdfBuffer;
 };
